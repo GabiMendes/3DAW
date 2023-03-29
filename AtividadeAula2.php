d5653d1c63e7a42eba504a8982069dfd717d4932
Gabriela Mendes <123849754+GabiMendes@users.noreply.github.com>
2023-03-28 22:52:40 -0300

    Add files via upload

---
+++ AtividadeAula2.php
@@ -0,0 +1,55 @@
+<!Doctype html>
+<html>
+    <head>
+        <title> Atividade Aula 2 </title>
+        <style>
+            table, tr, td, th{
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+    <h1>Atividade Aula 2</h1>
+        <?php
+        $nomes = array ("Ana", "Bia", "Catarina", "Duda");
+		$notas = array (8.9, 6.2, 9, 3);
+        //criar função para gerar
+		function avaliar_Notas($nomes, $notas)
+		{
+			if($notas<8)
+			{
+				echo "
+				<tr>
+					<td>$nomes</td>
+					<td>$notas</td>
+					<td>Reprovado</td>
+				</tr>
+				";
+			}else
+			{
+			echo "
+			<tr>
+				<td>$nomes</td>
+				<td>$notas</td>
+				<td>Aprovado</td>
+				
+			</tr>
+			";
+			}
+		}
+		?>
+		<table>
+		<tr>
+			<th>Nome:</th>
+			<th>Nota:</th>
+			<th>Situação:</th>
+		</tr>
+		<?php
+		for($i=0; $i<count($nomes); $i++)
+		{
+			avaliar_Notas($nomes[$i], $notas[$i]);
+		}
+		?>
+		</table>
+    </body>
+</html>
